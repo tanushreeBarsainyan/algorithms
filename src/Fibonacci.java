4124505a41442ab473d1abbdf7561740193314ef
tanushreeBarsainyan <tanubarsainyan@gmail.com>
2023-10-05 15:42:38 -0700

    Adding Fibonacci all 3 versions.

---
+++ src/Fibonacci.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class Fibonacci {
+    static HashMap<Integer, Integer> computedFib = new HashMap<>();
+
+    public static int fiboRecursion(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        return fiboRecursion(n - 1) + fiboRecursion(n - 2);
+    }
+
+    public static int fibRecursionWithMap(int n) {
+        computedFib.put(1, 1);
+        computedFib.put(2, 1);
+        if (n > 2) {
+            computedFib.put(n, (fibRecursionWithMap(n - 1) + fibRecursionWithMap(n - 2)));
+        }
+        return computedFib.get(n);
+    }
+
+    public static int fibRecursionWithArray(int n) {
+        int[] fibArray = new int[n];
+        fibArray[0] = 1;
+        fibArray[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+        return fibArray[n - 1];
+    }
+
+    public static int fibRecursionWithDP(int n) {
+        int curr = 1;
+        int prev = 1;
+        int result = 0;
+        for (int i = 3; i <= n; i++) {
+            result = curr + prev;
+            prev = curr;
+            curr = result;
+        }
+        return curr;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //fibonacci all methods
+        System.out.println(Fibonacci.fiboRecursion(11));
+        System.out.println(Fibonacci.fibRecursionWithMap(11));
+        System.out.println(Fibonacci.fibRecursionWithArray(11));
+        System.out.println(Fibonacci.fibRecursionWithDP(11));
+    }
+}
+
